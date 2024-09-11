4a16e468b54f14b838410d96f309c64bc5058b80
olegmakeienko <molva83@gmail.com>
2024-09-11 22:20:24 +0200

    Player in Main

--- main.cpp
+++ main.cpp
@@ -1,12 +1,23 @@
 #include <iostream>
 #include "Dice.h"
+#include "Player.h"
 
 using namespace std;
 
 int main()
 {
-    Dice dice(6);
-    dice.roll();
-    cout << dice.getValue() << endl;
+    // Dice dice(6);
+    // dice.roll();
+    // cout << dice.getValue() << endl;
+
+    Player player("Johan");
+    player.addDie(6);
+    player.addDie(6);
+    player.addDie(6);
+
+    player.rollDice();
+
+    cout << player.getPlayerName() << " rolled a total value of: " << player.getTotalValue() << endl;
+
     return 0;
 }
