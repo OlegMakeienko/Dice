d3813f77b290fa6e5ace5e9ba77ac84456792331
olegmakeienko <molva83@gmail.com>
2024-09-13 12:21:32 +0200

    Refactor: Split Dice class into header and implementation files
    - Created Die.h for the Die class declarations.
    - Created Die.cpp for the Die class method implementations.
    - Moved the constructor and method definitions from the original source to Die.cpp.
    - Ensured proper includes and header guards in Die.h.

--- Dice.h
+++ Dice.h
@@ -12,35 +12,15 @@ using namespace std;
 
 class Dice {
 private:
-    int sides;
-    int value;
+    int sides{};
+    int value{};
 
 public:
-    Dice(int s = 6) : sides(s), value(1) {  //initializer list
+    Dice(int s = 6);
 
-/*
-        *Initialisering vs. Tilldelning:
+    void roll();
 
-        Initialisering: När en variabel initialiseras, skapas den direkt med ett specifikt värde.
-            Till exempel, för variabeln name skapas ett std::string-objekt direkt med det önskade värdet.
-        Tilldelning: Om vi istället tilldelar ett värde till variabeln i konstruktorns kropp
-            (t.ex. name = playerName;), skapas först en standardinstans av name,
-            och sedan tilldelas det nya värdet. Detta kan leda till extra arbete och onödiga kostnader.
- */
-
-
-        srand(time(nullptr)); //initialisation random point
-    }
-
-    void roll() {
-        value = (rand() % sides) + 1;
-    }
-
-    int getValue() const { //const betyder att vi ska inte ändra value bara läser det
-        return value;
-    }
+    int getValue() const;
 };
-
-
 
 #endif //DICE_H
